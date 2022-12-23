b97679113ca20c5aea8d699241affd36f539c55b
abdelhaq-97 <nouhiabdelhak5@gmail.com>
2022-12-23 11:07:59 +0100

    fix

--- app/Routes/Users.route.js
+++ app/Routes/Users.route.js
@@ -6,5 +6,6 @@ const UsersController = require('../Controllers/Users.Controller');
 //Get a list of all products
 router.get('/GetAllUser', UsersController.GetAllUsers)
 router.post('/RegisterUser', UsersController.RegisterUser)
+router.post('/LoginUser', UsersController.LoginUser)
 
 module.exports = router;
